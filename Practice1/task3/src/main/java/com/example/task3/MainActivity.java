b12cc724f71dc5a080b6dc916ef7087a2f6ce191
user <kosyanip@gmail.com>
2021-05-20 21:38:26 +0300

    Finished first practice

---
+++ Practice1/task3/src/main/java/com/example/task3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.task3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("Kappa keppo kippa");
+        Button button = findViewById(R.id.button13);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
+    }
+}
